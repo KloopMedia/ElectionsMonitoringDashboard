8816f203b5720a21f6ce33c434330ed8673b8f84
aTNa <atay.narynov@gmail.com>
2020-09-23 16:32:59 +0600

    Updates

---
+++ precache-manifest.40c205d01e2e73553188cdfa5b6453bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13b54500968635645f0b6e1943a22a3c",
+    "url": "/ElectionsMonitoringDashboard/index.html"
+  },
+  {
+    "revision": "c8d12e4139968ada1405",
+    "url": "/ElectionsMonitoringDashboard/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "6de4ef4c788243f330b6",
+    "url": "/ElectionsMonitoringDashboard/static/js/2.0ee9122c.chunk.js"
+  },
+  {
+    "revision": "e1a2b590289b8b0a443a0c2fb2738ca4",
+    "url": "/ElectionsMonitoringDashboard/static/js/2.0ee9122c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8d12e4139968ada1405",
+    "url": "/ElectionsMonitoringDashboard/static/js/main.65430199.chunk.js"
+  },
+  {
+    "revision": "cba610a922fa501253d3",
+    "url": "/ElectionsMonitoringDashboard/static/js/runtime-main.c1703e19.js"
+  }
+]);
